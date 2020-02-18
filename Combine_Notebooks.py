9f4bd551cebb2d47c127cae57ed1c805f068e90d
Anderson <BMAnderson@mdanderson.org>
2020-02-18 09:13:19 -0600

    update merge

--- Combine_Notebooks.py
+++ Combine_Notebooks.py
@@ -3,5 +3,6 @@ __author__ = 'Brian M Anderson'
 from Workshop_Modules.Merge_Notebooks import merge_notebooks_function, os
 
 path_to_notebooks = os.path.join('.','Workshop_Modules')
+notebook_paths = [os.path.join('.','Download_Data.ipynb')] + [os.path.join('.\\Workshop_Modules',i) for i in ['DeepBox.ipynb','Data_Curation.ipynb','Liver_Model.ipynb']]
 out_path_name = os.path.join('.','Click_Me.ipynb')
-merge_notebooks_function(path_to_notebooks=path_to_notebooks,combined_notebook_path=out_path_name)
+merge_notebooks_function(notebook_paths=notebook_paths,notebook_output_path=out_path_name)
